7a9e63d4585e4d4bfe12693325c825daf41563ea
Камышенкова Татьяна Александровна <kamushenkova_t@da.lan>
2025-06-20 12:01:23 +0300

    Изменены файлы для прохождения теста

--- conftest.py
+++ conftest.py
@@ -1,30 +1,11 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-
-
-def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose language: en or ru")
-
+from selenium.webdriver.common.by import By
 
 @pytest.fixture(scope="function")
-def browser(request):
-    language = request.config.getoption("language")
-    browser = None
-    if language == "en":
-        print("\nstart chrome en browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': en})
-        browser = webdriver.Chrome(options=options)
-    elif language == "ru":
-        print("\nstart chrome ru browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': ru})
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should be en or ru")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
     browser.quit()
